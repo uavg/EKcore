fb60a3ae729f96721f57a195598cbf1d69d382e7
EKE00372 <eke00372@gmail.com>
2016-08-15 05:42:47 +0800

    update interface enhance mode list
    talking head frame resize
    order hall bar hide

---
+++ Interface/HideTalkingHead.lua
@@ -0,0 +1,18 @@
+-- License: Public Domain
+
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, addon)
+	if addon == "Blizzard_TalkingHeadUI" then
+		hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+			--TalkingHeadFrame:Hide()
+			TalkingHeadFrame:SetScale(0.7)
+			--TalkingHeadFrame:ClearAllPoints()
+			--TalkingHeadFrame:(point, uiparent, Point, x, y)
+		end)
+		self:UnregisterEvent(event)
+	end
+end
+
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", f.OnEvent)
