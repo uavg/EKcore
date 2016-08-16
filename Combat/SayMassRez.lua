f2f13a249a8171bdd99d967cb19df48d7ce4108e
EKE00372 <eke00372@gmail.com>
2016-08-16 16:25:11 +0800

    follow
    官方更新

--- Combat/SayMassRez.lua
+++ Combat/SayMassRez.lua
@@ -1,20 +1,35 @@
 local SayMassRez = CreateFrame("Frame")
+local timer = 0
 
 SayMassRez:SetScript("OnEvent",function(_,_,_,event, _,_,sourceName, _,_,_,_,_,_,spellId)
-	if ((spellId == 212040) or (spellId == 212051) or (spellId == 212036) or (spellId == 212048) or (spellId == 212056)
+	if ((spellId == 212036) or (spellId == 212040) or (spellId == 212048) or (spellId == 212051) or (spellId == 212056)
 	and (event  == "SPELL_CAST_START")
 	and (UnitInRaid(sourceName) or UnitInParty(sourceName)))
 	then
-		local num, group = GetNumGroupMembers(), "raid"
+		DEFAULT_CHAT_FRAME:AddMessage("Mass Rez Detected ["..sourceName.."]")
+		
+		if (timer < time()) then
+			local group, dead = "raid", 0
 			if (not IsInRaid()) then
 				group = "party"
+				if UnitIsDeadOrGhost("player") then
+					dead = 1
+				end
 			end
-		DEFAULT_CHAT_FRAME:AddMessage("Mass Rez Detected ["..sourceName.."]")
-		SendChatMessage(">> Mass Rez/群復 cast by ["..sourceName.."]", IsInRaid(LE_PARTY_CATEGORY_INSTANCE) and "INSTANCE_CHAT" or group)
-		
+
+			for i = 1, GetNumGroupMembers() do
+				if UnitIsDeadOrGhost(group..i) then
+					dead = dead + 1
+				end
+			end
+
+			if (dead > 0) then
+				SendChatMessage(">> Mass Rez/群復 cast by ["..sourceName.."],"..dead.." rezzable", IsInRaid(LE_PARTY_CATEGORY_INSTANCE) and "INSTANCE_CHAT" or group)
+				timer = time() + 10
+			end
+		end
 	end
 end)
 
-
 SayMassRez:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
---DEFAULT_CHAT_FRAME:AddMessage("SayMassRez loaded")
+DEFAULT_CHAT_FRAME:AddMessage("SayMassRez loaded")
