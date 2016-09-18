8ea393e233a2b3a54f84d52d8faddfc0bf124727
EKE00372 <eke00372@gmail.com>
2016-09-19 02:25:22 +0800

    由pmAlertFrame取代

--- Interface/AlertMover.lua
+++
@@ -1,6 +0,0 @@
-local function customFixAnchors(self, ...)
-AlertFrame:ClearAllPoints()
-AlertFrame:SetPoint("CENTER",UIParent,"CENTER",-400,0)
-end
-
-hooksecurefunc(AlertFrame,"UpdateAnchors", customFixAnchors)
